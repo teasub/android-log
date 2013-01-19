cf495aff1612112ff72bd23606a41eaba7c0566c
yanghui <yanghui1986527@gmail.com>
2013-01-19 11:58:09 +0800

    修改包名
    Signed-off-by: yanghui <yanghui1986527@gmail.com>

--- src/main/java/com/github/snowdream/android/Log.java
+++ src/main/java/com/github/snowdream/android/Log.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.snowdream.android;
+package com.github.snowdream.android;
 
 /**
  * Wrapper API for sending log output
